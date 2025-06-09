a81998b490bcdb8c5d6b33e90e998607a4f653a4
L6he <158562450+L6he@users.noreply.github.com>
2025-06-09 11:35:56 +0300

    w

---
+++ Flugg-FilmRegister/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Flugg_FilmRegister.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
